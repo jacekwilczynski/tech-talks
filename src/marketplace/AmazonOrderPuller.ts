5cdaff4417ae2227c6442e7af1044727a17446e6
Jacek Wilczynski <jwilczynski@notebooksbilliger.de>
2022-04-14 11:54:23 +0200

    split

--- src/marketplace/AmazonOrderPuller.ts
+++ src/marketplace/AmazonOrderPuller.ts
@@ -4,22 +4,34 @@ import { createOrder } from '../order/order-repository';
 import Money from 'js-money';
 import Currencies from 'js-money/lib/currency';
 import { getProductById, updateProduct } from '../product/product-repository';
+import { Order } from '../order/Order';
 
 export default class AmazonOrderPuller {
     private readonly httpClient = new HttpClient(amazonConfig);
 
     public async pullOrders(createdAfter: Date): Promise<void> {
-        const orders = new Orders(this.httpClient);
+        const orders = await this.fetchOrdersFromAmazon(createdAfter);
 
-        const [ordersList] = await orders.listOrders({
+        for (const order of orders) {
+            createOrder(order);
+            await this.updateProductsStock(order);
+        }
+    }
+
+    private async fetchOrdersFromAmazon(createdAfter: Date): Promise<Order[]> {
+        const amazonOrders = new Orders(this.httpClient);
+
+        const [ordersList] = await amazonOrders.listOrders({
             MarketplaceId: [amazonConfig.marketplace.id],
             CreatedAfter: createdAfter,
         });
 
-        for (const order of ordersList.Orders) {
-            const [orderItemList] = await orders.listOrderItems(order);
+        const shopOrders: Order[] = [];
 
-            createOrder({
+        for (const order of ordersList.Orders) {
+            const [orderItemList] = await amazonOrders.listOrderItems(order);
+
+            shopOrders.push({
                 id: order.AmazonOrderId,
                 lineItems: orderItemList.OrderItems.map(orderItem => ({
                     productId: String(orderItem.SellerSKU),
@@ -27,14 +39,16 @@ export default class AmazonOrderPuller {
                     unitPrice: Money.fromDecimal(orderItem.ItemPrice?.Amount || 0, Currencies.EUR),
                 })),
             });
+        }
 
-            for (const orderItem of orderItemList.OrderItems) {
-                const productId = String(orderItem.SellerSKU);
+        return shopOrders;
+    }
 
-                const product = await getProductById(productId);
-                product.stock -= orderItem.QuantityOrdered;
-                updateProduct(product);
-            }
+    private async updateProductsStock(order: Order): Promise<void> {
+        for (const orderItem of order.lineItems) {
+            const product = await getProductById(orderItem.productId);
+            product.stock -= orderItem.quantity;
+            updateProduct(product);
         }
     }
 }
